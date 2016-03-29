b2ce7cbb407d525791c7756fd789d651e51ed3fe
Ghabri Djalel <Ghabri Djalel>
2016-03-29 09:54:53 +0100

    djalel commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,64 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700|Merriweather:400,700' rel='stylesheet' type='text/css'>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+  	
+	<title>Les Aigles Arabes</title>
+</head>
+<body>
+	<main>
+		<h1>Les Aigles Arabes</h1>
+
+		<p>Est un expert d'immobilière.</p>
+	</main>
+
+	<a href="#cd-nav" class="cd-nav-trigger">Menu 
+		<span class="cd-nav-icon"></span>
+
+		<svg x="0px" y="0px" width="54px" height="54px" viewBox="0 0 54 54">
+			<circle fill="transparent" stroke="#656e79" stroke-width="1" cx="27" cy="27" r="25" stroke-dasharray="157 157" stroke-dashoffset="157"></circle>
+		</svg>
+	</a>
+	
+	<div id="cd-nav" class="cd-nav">
+		<div class="cd-navigation-wrapper">
+			<div class="cd-half-block">
+				<h2>Menu</h2>
+
+				<nav>
+					<ul class="cd-primary-nav">
+						<li><a href="#0" class="selected">ACCUEIL</a></li>
+						<li><a href="#0">A PROPOS</a></li>
+						<li><a href="#0">NOS PRODUITS</a></li>
+						<li><a href="#0">CONTACT</a></li>
+						
+					</ul>
+				</nav>
+			</div><!-- .cd-half-block -->
+			
+			<div class="cd-half-block">
+				<address>
+					<ul class="cd-contact-info">
+						<li><a href="mailto:info@myemail.co">info@aigles-arabes.com</a></li>
+						<li>216-12345678</li>
+						<li>
+							<span>Adresse</span>
+							<span>Immeuble</span>
+							<span>Menzah, Tunis</span>
+						</li>
+					</ul>
+				</address>
+			</div> <!-- .cd-half-block -->
+		</div> <!-- .cd-navigation-wrapper -->
+	</div> <!-- .cd-nav -->
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
